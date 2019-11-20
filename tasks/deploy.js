672ceab68ee40d808b5635821691996eeb596dc0
Andrii Suprun <smiranin007@gmail.com>
2019-11-20 22:35:29 +0200

    Auto-commit

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -31,7 +31,7 @@ module.exports = function (options) {
             .pipe(through2(
                 function (file, code, cb) {
                     shell.exec('find . | grep -v .git | grep -v .gitignore | grep -v node_modules | grep -v index.html | xargs rm -rf ');
-                    shell.exec('git add --all && git commit -m "deploy" && git push origin gh-pages');
+                    shell.exec('git add --all && git commit -m "deploy" && git pull origin gh-pages && git push origin gh-pages');
                     cb(null, file);
                 }
             ))
