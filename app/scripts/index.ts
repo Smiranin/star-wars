d520965449f4b3b30aca30743468961c0e318993
Andrii Suprun <smiranin007@gmail.com>
2019-11-20 23:30:21 +0200

    deploy

--- app/scripts/index.ts
+++
@@ -1,7 +0,0 @@
-import {App} from "./App";
-
-if(document.querySelector(".js-calendar-graph-svg")) {
-    let app = new App({
-        svg: document.querySelector(".js-calendar-graph-svg")
-    })
-} else {(<any>window).location="https://smiranin.github.io/star-wars/"}
